e8828414ed572c8b7e5a8c6675e4891889c05b3b
Saurabh Maurya <sm0107870@gmail.com>
2023-11-22 18:15:43 +0530

    added @LoadBalanced on restTemplate

--- RatingService/src/main/java/com/rating/service/config/MyConfig.java
+++ RatingService/src/main/java/com/rating/service/config/MyConfig.java
@@ -1,6 +1,8 @@
 package com.rating.service.config;
 
 
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class MyConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
